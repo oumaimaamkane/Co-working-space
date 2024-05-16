4730be591dcf23e29557ae0ecc067f432a45768a
amine benmade <aminebenmade1999@gmail.com>
2024-05-16 16:44:35 +0100

    add CRUD of Categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,62 +5,50 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Http\Requests\StoreCategoryRequest;
 use App\Http\Requests\UpdateCategoryRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
+    public function index(){
+        $categories = Category::all();
+        return response()->json($categories);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+    public function store(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+        ]);
+
+        if($validator->fails()){
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+        $category = Category::create([
+            'name' => $request->input('name'),
+        ]);
+        return response()->json($category, 201);
+
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreCategoryRequest $request)
-    {
-        //
+    public function update(Request $request, $id){
+
+        $category = Category::findOrFail($id);
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+        ]);
+        if($validator->fails()){
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+        $category->update($request->all());
+        return response()->json($category, 200);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Category $category)
-    {
-        //
-    }
+    public function destroy(Request $request, $id){
+        $category = Category::findOrFail($id);
+        $category->delete();
+        return response()->json(null, 204);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Category $category)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateCategoryRequest $request, Category $category)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Category $category)
-    {
-        //
     }
 }
