d500311ea5f189f7a2475eb3b4cf637315c84f18
Echaftech23 <rachidchaf2001@gmail.com>
2024-05-16 17:38:43 +0100

    updated categorySeeder.

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,16 +2,9 @@
 
 namespace Database\Seeders;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-=======
 use App\Models\Category;
->>>>>>> 8132f1a407d9c370700bf9de3b6bbadff68b9b8e
-=======
 
-use App\Models\Category;
->>>>>>> aab833e1c066bfefcb915b8f46c5d369bd84470a
+
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
@@ -21,20 +14,12 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
-        //
-=======
-=======
->>>>>>> aab833e1c066bfefcb915b8f46c5d369bd84470a
+
+
         $categories = ['Bureau Privee', 'Poste du travail partager', 'Salle de reunion'];
 
         foreach ($categories as $category) {
             Category::create(['name' => $category]);
         }
-<<<<<<< HEAD
->>>>>>> 8132f1a407d9c370700bf9de3b6bbadff68b9b8e
-=======
->>>>>>> aab833e1c066bfefcb915b8f46c5d369bd84470a
     }
 }
